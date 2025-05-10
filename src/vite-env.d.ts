8d8b2419a757bf6e75fc866abd223bad643b0ae6
Andres Agudelo <anagudelogu@gmail.com>
2025-05-09 21:55:55 -0500

    feat: add TS intellisense to vite env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,14 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ViteTypeOptions {
+  // By adding this line, you can make the type of ImportMetaEnv strict
+  // to disallow unknown keys.
+  strictImportMetaEnv: unknown
+}
+
+interface ImportMetaEnv {}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
