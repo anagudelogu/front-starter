8031620b1b41f5776facaff30da20d69062ddd49
Andres Agudelo <anagudelogu@gmail.com>
2025-05-09 21:55:26 -0500

    feat: add svgr plugin to convert svgs to react components

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), svgr(), react()],
 })
